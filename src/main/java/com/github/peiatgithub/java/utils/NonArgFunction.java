22dbaac5c740c93a116e95faf58d38a6594fe15d
pei <pei.shi@outlook.com>
2018-05-05 02:14:53 +0200

    The NonArgFunction

---
+++ src/main/java/com/github/peiatgithub/java/utils/NonArgFunction.java
@@ -0,0 +1,16 @@
+package com.github.peiatgithub.java.utils;
+
+/**
+ * 
+ * Represents a function takes no arg and has no return value.
+ * 
+ * @author pei
+ *
+ */
+@FunctionalInterface
+
+public interface NonArgFunction {
+	
+    public void doSth();
+
+}
