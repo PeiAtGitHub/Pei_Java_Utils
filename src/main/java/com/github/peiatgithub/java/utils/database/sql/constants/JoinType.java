dd5907b4d6b0c0484b80275b901a014aae990b66
pei <pei.shi@outlook.com>
2018-06-30 00:32:46 +0200

    Added Enum JoinType

---
+++ src/main/java/com/github/peiatgithub/java/utils/database/sql/constants/JoinType.java
@@ -0,0 +1,26 @@
+package com.github.peiatgithub.java.utils.database.sql.constants;
+
+
+/**
+ * 
+ * @author pei
+ *
+ */
+public enum JoinType {
+    
+    INNER_JOIN("INNER JOIN"),
+    LEFT_JOIN("LEFT JOIN"),
+    RIGHT_JOIN("RIGHT JOIN"),
+    FULL_OUTER_JOIN("FULL OUTER JOIN");
+   
+    private String text;
+
+    JoinType(String text) {
+        this.text = text;
+    }
+
+    public String text() {
+        return this.text;
+    }
+
+}
